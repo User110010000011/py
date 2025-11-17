9e3a5dd55a433df7f6dd241a1cab852cca10a3c9
shailesh <shaileshm1408@gmail.com>
2025-11-17 19:47:50 +0530

    Revert "minor changes committed"
    This reverts commit e03d29c3c0606d8e9d4d89e177e0ceeeff6a66bc.

--- list_problems/vowels.py
+++ list_problems/vowels.py
@@ -1,9 +1,6 @@
-"""this is a vowel length count"""
 import re
 from collections import deque
-words = ["banana", "mapleau","apple", 
-         "grape","epple","aepple",
-         "capleau", "kIwI", "strawberry","AEIOU","aeioua","aapleau"]
+words = ["banana", "mapleau","apple", "grape","epple","aepple","capleau", "kIwI", "strawberry","AEIOU","aeioua","aapleau"]
 word_dict={}
 dq=deque()
 max_val=0
@@ -12,30 +9,35 @@ for i in words:
     count=0
     for j in i:
         if re.search("[aeiouAEIOU]+",j):
-            count+=1
+             count+=1
         word_dict[i]=count
 # print(word_dict)
 # obj=sorted(word_dict)     
 # print(obj)
 for key,val in word_dict.items():
-    if val>max_val and val>start_val:
-        dq.appendleft(key)
-        start_val=val
-        max_val=val 
-    elif val>max_val and val<=start_val:
-        cnt=0
-        while start_val>val:
-            start_val-=1
-            cnt+=1               
-        dq.insert(cnt,key)   
+     if val>max_val and val>start_val:
+          dq.appendleft(key)
+          start_val=val
+          max_val=val
+     elif val>max_val and val<=start_val:
+               cnt=0
+               while start_val>val:
+                       start_val-=1
+                       cnt+=1
+                           
+               dq.insert(cnt,key)   
      # elif max_val==val:
      #        pass 
-    else:
-        dq.append(key)
-        max_val=val
+     else:
+          dq.append(key)
+          max_val=val
+
 res=[]
 
 for i in dq:
-    res.append(i)
+     res.append(i)
 print(res)
-  
+
+
+
+      
