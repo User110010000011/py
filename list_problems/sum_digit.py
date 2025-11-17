39180a68b93b7f7a4e6cb1a97178b0fd6d0a05e6
shailesh <shaileshm1408@gmail.com>
2025-11-17 19:50:45 +0530

    Revert " checking for revert"
    This reverts commit 7fcd7f7015c3a3cf5df878c0687f58a6bc4f6e70.

--- list_problems/sum_digit.py
+++ list_problems/sum_digit.py
@@ -1,19 +1,18 @@
-"""sum digits """
 import math
-def sum_digits(num,sum):
-    """this is used to sum digits """
-    if num<10:
-        return num+sum
-    else:
-        q=num/10
-        r=num%10
+def sum_digits(num:int,sum:int):
+   if num<10:
+      return num+sum
+   else:
+     q=num/10
+     r=num%10
    #  print(q,r)
-        sum+=r
-    return sum_digits(math.floor(q),sum)
+     sum+=r
+     return sum_digits(math.floor(q),sum)
 
 
 numbers = [11,20,56,89]
-sum2=[]
+sum=[]
 for i in numbers:
-    sum2.append(sum_digits(i,0)) 
-print(sorted(sum2))
+  sum.append(sum_digits(i,0)) 
+ 
+print(sorted(sum))
