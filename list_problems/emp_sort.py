b43933935bbe611491459f9e6b05a64c56b915d4
shailesh <shaileshm1408@gmail.com>
2025-11-18 19:57:18 +0530

    changes done in emp_sort.py

--- list_problems/emp_sort.py
+++ list_problems/emp_sort.py
@@ -11,23 +11,12 @@ employees = [
 ]
 
 
-result = deque()
-for elem in employees:
-    inserted = False
-    for i in range(len(result)):
-        if elem['salary'] >= result[i]['salary']:
-            result.insert(i, elem)
-            inserted = True
-            break
-        elif elem['salary']==result[i]['salary']:
-            if ord(elem['name'][0])>ord(result[i]['name']):
-                result.insert(i,elem)
-                inserted=True
-            else:
-                result.append(elem)
-                inserted=True
-    if not inserted:
-        result.append(elem)
-
-
+result = []
+new_dict={}
+for idx,val in enumerate(employees):
+    new_dict[idx]=val['salary']
+new_dict=sorted(new_dict,key=lambda x:new_dict[x],reverse=True)
+for idx ,val in enumerate(employees):
+    result.append(employees[new_dict[idx]])
 print(result)
+
